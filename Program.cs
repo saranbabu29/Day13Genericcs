3abff21ca07014c28a36ff69ff6e350faa714f39
lingesh2836 <sharanraj982@gmail.com>
2022-08-21 12:37:38 +0530

    Using Generics

--- Program.cs
+++ Program.cs
@@ -11,35 +11,18 @@ namespace GenericsExample
             double[] Array2 = { 1.1, 1.2, 1.3, 1.4, 1.5, 1.6 };
             char[] Array3 = new char[] { 's', 'h', 'a', 'r', 'a', 'n' };
 
-            Program.toPrint(Array1);
-            Program.toPrint(Array2);
-            Program.toPrint(Array3);
+            Program.toPrint<int>(Array1);
+            Program.toPrint<double>(Array2);
+            Program.toPrint<Char>(Array3);
 
         }
-        public static void toPrint(int[] inputArray1)
+        public static void toPrint<T>(T[] inputArray1)
         {
-            foreach (int element in inputArray1)
+            foreach (var element in inputArray1)
             {
                 Console.WriteLine(element);
             }
             Console.WriteLine("----------------------------------");
         }
-        public static void toPrint(double[] inputArray2)
-        {
-            foreach (double element in inputArray2)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("----------------------------------");
-        }
-        public static void toPrint(char[] inputArray3)
-        {
-            foreach (char element in inputArray3)
-            {
-                Console.Write(element);
-            }
-            Console.WriteLine("");
-        }
-
     }
 }
